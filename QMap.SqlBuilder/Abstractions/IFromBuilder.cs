368abbe00ecac1993375b7944c2496e85973327f
ADRN_NKT <combinerk@gmail.com>
2024-09-22 16:22:28 +0300

    Fix alises genrations for next statements

--- QMap.SqlBuilder/Abstractions/IFromBuilder.cs
+++ QMap.SqlBuilder/Abstractions/IFromBuilder.cs
@@ -1,7 +1,11 @@
-﻿namespace QMap.SqlBuilder.Abstractions
+﻿using System.Collections.Concurrent;
+
+namespace QMap.SqlBuilder.Abstractions
 {
     public interface IFromBuilder : IQueryBuilder
     {
         public IFromBuilder BuildFrom(ISelectBuilder quryBuilder, Type entity, params Type[] entities);
+
+        ConcurrentDictionary<string, string> Aliases { get; }
     }
 }
